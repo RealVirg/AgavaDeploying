0f637b615c5b790f6d00bb0e796fc79172e9cc4d
Исаков Юрий Алексеевич <userija@office.kb-agava.ru>
2022-06-15 16:53:58 +0500

    device-reworked

--- agava/app/account/admin.py
+++ agava/app/account/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
-from .models import AccountModel, AccountProjectModel, AccountPermissionsModel
+from .models import AccountModel, AccountProjectModel, AccountPermissionsModel,\
+    AccountParameterModel, AccountModbusRegisterModel
 
 
 admin.site.register(AccountModel)
 admin.site.register(AccountProjectModel)
 admin.site.register(AccountPermissionsModel)
+admin.site.register(AccountParameterModel)
+admin.site.register(AccountModbusRegisterModel)
