1e1988215ce8ef5495517973f6b0a0f1d3b34952
Исаков Юрий Алексеевич <userija@office.kb-agava.ru>
2022-06-24 10:58:47 +0500

    celery-rabbit-pub/sub

--- agava/app/account/models.py
+++ agava/app/account/models.py
@@ -14,9 +14,9 @@ class AccountModel(models.Model):
 
 class AccountPermissionsModel(models.Model):
     account = models.ForeignKey(AccountModel, on_delete=models.CASCADE, null=True)
-    admin = models.CharField(default='Нет прав')  # Чтение и запись, Чтение, Нет прав
-    device = models.CharField(default='Нет прав')
-    tables = models.CharField(default='Нет прав')
+    admin = models.CharField(default='Нет прав', max_length=100, label="")  # Чтение и запись, Чтение, Нет прав
+    device = models.CharField(default='Нет прав', max_length=100, label="")
+    tables = models.CharField(default='Нет прав', max_length=100, label="")
 
     def __str__(self):
         return self.account.user.username
