cf3cef58cc29aa6a036d26d5d3604f8c323dd42f
Исаков Юрий Алексеевич <userija@office.kb-agava.ru>
2022-06-27 15:36:29 +0500

    hi

--- agava/app/static/mainpage/js/widgets.js
+++ agava/app/static/mainpage/js/widgets.js
@@ -1,9 +1,9 @@
-function f(csv){
+function f(csv, target, wdth, hght){
     var margin = {top: 10, right: 30, bottom: 30, left: 60},
-        width = 1000 - margin.left - margin.right,
-        height = 400 - margin.top - margin.bottom;
+        width = wdth - margin.left - margin.right,
+        height = hght - margin.top - margin.bottom;
 
-    var svg = d3.select("#my_dataviz")
+    var svg = d3.select(target)
       .append("svg")
         .attr("width", width + margin.left + margin.right)
         .attr("height", height + margin.top + margin.bottom)
