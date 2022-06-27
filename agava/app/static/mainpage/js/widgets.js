60170e9eb7f45adf2bbc9ffbc8ab572943489a3e
Исаков Юрий Алексеевич <userija@office.kb-agava.ru>
2022-06-27 11:28:21 +0500

    hi

--- agava/app/static/mainpage/js/widgets.js
+++ agava/app/static/mainpage/js/widgets.js
@@ -2,7 +2,6 @@ const margin = {top: 10, right: 30, bottom: 30, left: 60},
     width = 1000 - margin.left - margin.right,
     height = 400 - margin.top - margin.bottom;
 
-// append the svg object to the body of the page
 const svg = d3.select("#my_dataviz")
   .append("svg")
     .attr("width", width + margin.left + margin.right)
@@ -11,19 +10,14 @@ const svg = d3.select("#my_dataviz")
     .attr("transform",
           `translate(${margin.left}, ${margin.top})`);
 
-//Read the data
-
 d3.csv("https://raw.githubusercontent.com/holtzy/data_to_viz/master/Example_dataset/3_TwoNumOrdered_comma.csv",
 
-  // When reading the csv, I must format variables:
   function(d){
     return { date : d3.timeParse("%Y-%m-%d")(d.date), value : d.value }
   }).then(
 
-  // Now I can use this dataset:
   function(data) {
 
-    // Add X axis --> it is a date format
     const x = d3.scaleTime()
       .domain(d3.extent(data, function(d) { return d.date; }))
       .range([ 0, width ]);
@@ -31,14 +25,12 @@ d3.csv("https://raw.githubusercontent.com/holtzy/data_to_viz/master/Example_data
       .attr("transform", `translate(0, ${height})`)
       .call(d3.axisBottom(x));
 
-    // Add Y axis
     const y = d3.scaleLinear()
       .domain([0, d3.max(data, function(d) { return +d.value; })])
       .range([ height, 0 ]);
     yAxis = svg.append("g")
       .call(d3.axisLeft(y));
 
-    // Add a clipPath: everything out of this area won't be drawn.
     const clip = svg.append("defs").append("svg:clipPath")
         .attr("id", "clip")
         .append("svg:rect")
@@ -47,19 +39,16 @@ d3.csv("https://raw.githubusercontent.com/holtzy/data_to_viz/master/Example_data
         .attr("x", 0)
         .attr("y", 0);
 
-    // Add brushing
-    const brush = d3.brushX()                   // Add the brush feature using the d3.brush function
-        .extent( [ [0,0], [width,height] ] )  // initialise the brush area: start at 0,0 and finishes at width,height: it means I select the whole graph area
-        .on("end", updateChart)               // Each time the brush selection changes, trigger the 'updateChart' function
+    const brush = d3.brushX()
+        .extent( [ [0,0], [width,height] ] )
+        .on("end", updateChart)
 
-    // Create the line variable: where both the line and the brush take place
     const line = svg.append('g')
       .attr("clip-path", "url(#clip)")
 
-    // Add the line
     line.append("path")
       .datum(data)
-      .attr("class", "line")  // I add the class line to be able to modify this line later on.
+      .attr("class", "line")
       .attr("fill", "none")
       .attr("stroke", "steelblue")
       .attr("stroke-width", 1.5)
@@ -68,32 +57,27 @@ d3.csv("https://raw.githubusercontent.com/holtzy/data_to_viz/master/Example_data
         .y(function(d) { return y(d.value) })
         )
 
-    // Add the brushing
     line
       .append("g")
         .attr("class", "brush")
         .call(brush);
 
-    // A function that set idleTimeOut to null
     let idleTimeout
     function idled() { idleTimeout = null; }
 
-    // A function that update the chart for given boundaries
     function updateChart(event,d) {
 
-      // What are the selected boundaries?
       extent = event.selection
 
-      // If no selection, back to initial coordinate. Otherwise, update X axis domain
+
       if(!extent){
-        if (!idleTimeout) return idleTimeout = setTimeout(idled, 350); // This allows to wait a little bit
+        if (!idleTimeout) return idleTimeout = setTimeout(idled, 350);
         x.domain([ 4,8])
       }else{
         x.domain([ x.invert(extent[0]), x.invert(extent[1]) ])
-        line.select(".brush").call(brush.move, null) // This remove the grey brush area as soon as the selection has been done
+        line.select(".brush").call(brush.move, null)
       }
 
-      // Update axis and line position
       xAxis.transition().duration(1000).call(d3.axisBottom(x))
       line
           .select('.line')
@@ -105,7 +89,6 @@ d3.csv("https://raw.githubusercontent.com/holtzy/data_to_viz/master/Example_data
           )
     }
 
-    // If user double click, reinitialize the chart
     svg.on("dblclick",function(){
       x.domain(d3.extent(data, function(d) { return d.date; }))
       xAxis.transition().call(d3.axisBottom(x))
@@ -117,5 +100,4 @@ d3.csv("https://raw.githubusercontent.com/holtzy/data_to_viz/master/Example_data
           .y(function(d) { return y(d.value) })
       )
     });
-
 })
