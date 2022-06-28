a308d64944d9ee6de6abda23aed8219e944d4e2d
Исаков Юрий Алексеевич <userija@office.kb-agava.ru>
2022-06-28 10:31:11 +0500

    hi

---
+++ agava/app/static/mainpage/js/widgets_v4.js
@@ -0,0 +1,160 @@
+function f(csv, target, wdth, hght){
+  var margin = {top: 10, right: 30, bottom: 30, left: 60},
+      width = wdth - margin.left - margin.right,
+      height = hght - margin.top - margin.bottom;
+
+  var svg = d3.select(target)
+    .append("svg")
+      .attr("width", width + margin.left + margin.right)
+      .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+      .attr("transform",
+            "translate(" + margin.left + "," + margin.top + ")");
+
+  d3.csv(csv,
+
+    function(d){
+      return { date : d3.timeParse("%Y-%m-%d")(d.date), value : d.value }
+    },
+
+    function(data) {
+
+      var x = d3.scaleTime()
+        .domain(d3.extent(data, function(d) { return d.date; }))
+        .range([ 0, width ]);
+      xAxis = svg.append("g")
+        .attr("transform", "translate(0," + height + ")")
+        .call(d3.axisBottom(x));
+
+      var y = d3.scaleLinear()
+        .domain([0, d3.max(data, function(d) { return +d.value; })])
+        .range([ height, 0 ]);
+      yAxis = svg.append("g")
+        .call(d3.axisLeft(y));
+
+      var clip = svg.append("defs").append("svg:clipPath")
+          .attr("id", "clip")
+          .append("svg:rect")
+          .attr("width", width )
+          .attr("height", height )
+          .attr("x", 0)
+          .attr("y", 0);
+
+      var brush = d3.brushX()
+          .extent( [ [0,0], [width,height] ] )
+          .on("end", updateChart)
+
+      var line = svg.append('g')
+        .attr("clip-path", "url(#clip)")
+
+      line.append("path")
+        .datum(data)
+        .attr("class", "line")
+        .attr("fill", "none")
+        .attr("stroke", "steelblue")
+        .attr("stroke-width", 1.5)
+        .attr("d", d3.line()
+          .x(function(d) { return x(d.date) })
+          .y(function(d) { return y(d.value) })
+          )
+
+      line
+        .append("g")
+          .attr("class", "brush")
+          .call(brush);
+
+      var idleTimeout
+      function idled() { idleTimeout = null; }
+
+      function updateChart() {
+
+        extent = d3.event.selection
+
+        if(!extent){
+          if (!idleTimeout) return idleTimeout = setTimeout(idled, 350);
+          x.domain([ 4,8])
+        }else{
+          x.domain([ x.invert(extent[0]), x.invert(extent[1]) ])
+          line.select(".brush").call(brush.move, null)
+        }
+
+        xAxis.transition().duration(1000).call(d3.axisBottom(x))
+        line
+            .select('.line')
+            .transition()
+            .duration(1000)
+            .attr("d", d3.line()
+              .x(function(d) { return x(d.date) })
+              .y(function(d) { return y(d.value) })
+            )
+      }
+    var bisect = d3.bisector(function(d) { return d.date; }).left;
+
+    var focus = svg
+      .append('g')
+      .append('line')
+        .style("fill", "none")
+        .attr("stroke", "black")
+        .style("opacity", 0)
+    var foc = svg
+      .append('g')
+      .append('line')
+        .style("fill", "none")
+        .attr("stroke", "black")
+        .style("opacity", 0)
+
+    var focusText = svg
+      .append('g')
+      .append('text')
+        .style("opacity", 0)
+        .attr("text-anchor", "left")
+        .attr("alignment-baseline", "middle")
+
+      svg.on("dblclick",function(){
+        x.domain(d3.extent(data, function(d) { return d.date; }))
+        xAxis.transition().call(d3.axisBottom(x))
+        line
+          .select('.line')
+          .transition()
+          .attr("d", d3.line()
+            .x(function(d) { return x(d.date) })
+            .y(function(d) { return y(d.value) })
+        )
+      })
+        .on('mouseover', function(){
+            foc.style("opacity", 1)
+            focus.style("opacity", 1)
+            focusText.style("opacity",1)
+         })
+         .on('mousemove', function(){
+          var x0 = x.invert(d3.mouse(this)[0]);
+          var i = bisect(data, x0, 1);
+          selectedData = data[i];
+          format = d3.timeFormat("%Y-%m-%d");
+          var change_location_x = 15;
+          if (x(selectedData.date) > (width / 2)){
+              change_location_x = -220;
+            }
+          foc
+            .attr("x1", 0)
+            .attr("y1", y(selectedData.value))
+            .attr("x2", width)
+            .attr("y2", y(selectedData.value));
+          focus
+            .attr("x1", x(selectedData.date))
+            .attr("y1", 0)
+            .attr("x2", x(selectedData.date))
+            .attr("y2", height);
+          focusText
+            .html("Date:" + format(selectedData.date) + "  -  " + "Value:" + selectedData.value)
+            .attr("x", x(selectedData.date)+change_location_x)
+            .attr("y", y(selectedData.value)-10);
+         })
+         .on('mouseout', function(){
+          foc.style("opacity", 0)
+          focus.style("opacity", 0)
+          focusText.style("opacity", 0)
+         });
+
+  })
+}
