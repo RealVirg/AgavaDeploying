ee5e5dc9e701416bba117f2c25240c969e4ac96c
Исаков Юрий Алексеевич <userija@office.kb-agava.ru>
2022-07-07 14:03:19 +0500

    db

--- agava/app/account/forms.py
+++ agava/app/account/forms.py
@@ -73,13 +73,19 @@ class DeleteDashboardForm(forms.Form):
             AccountDashboardModel.objects.filter(project=get_object_or_404(AccountProjectModel, id=id)), label='')
 
 
-class CreateWidgetForm(forms.Form):
+class ChooseDeviceForm(forms.Form):
     def __init__(self, id, *args, **kwargs):
-        super(CreateWidgetForm, self).__init__(*args, **kwargs)
+        super(ChooseDeviceForm, self).__init__(*args, **kwargs)
         self.fields['device'] = forms.ModelChoiceField(AccountDevicesModel.objects.filter(
             project=get_object_or_404(AccountProjectModel, id=id)), label="")
+
+
+class CreateWidgetForm(forms.Form):
+    def __init__(self, id, *args, **kwargs):
+        f = ChooseDeviceForm(id)
+        super(CreateWidgetForm, self).__init__(*args, **kwargs)
         self.fields['parameter'] = forms.ModelChoiceField(AccountParameterModel.objects.filter(
-            device=get_object_or_404(AccountDevicesModel.objects.get(id=self.cleaned_data['device'].id))), label="")
+            device=get_object_or_404(AccountDevicesModel.objects.get(id=f.cleaned_data['device'].id))), label="")
     name = forms.CharField(label='', widget=forms.TextInput(attrs={'placeholder': "Имя виджета"}))
     wdth = forms.IntegerField(label='', widget=forms.NumberInput(attrs={'placeholder': "Ширина виджета"}))
     hght = forms.IntegerField(label='', widget=forms.NumberInput(attrs={'placeholder': "Высота виджета"}))
